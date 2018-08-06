31793034fb883cb267b30550d14633191314d6aa
Roop Pal <roop.pal@ibm.com>
2018-08-05 22:26:02 -0400

    Create basic React app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <h1>Hello, world!</h1>;
+    }
+}
+
+export default App;
